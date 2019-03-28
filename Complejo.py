217566076794d13c391dabcdbbea316a53f30a58
julianamendez11 <46967761+julianamendez11@users.noreply.github.com>
2019-03-28 07:17:07 -0500

    Create Complejo.py

---
+++ Complejo.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Complejo():
+    def __init__(self,real,imaginario):
+        self.imaginario=imaginario
+        self.real=real
+        
+    def conjugado():
+        return -imaginario
+    
+    def calcula_norma():
+        return np.sqrt(imaginario**2 + real**2)
+            
+    def pow(n):
+        return (real+imaginario)**n
